3551d054d9e005c443dab73c2619a67d1325f93b
ZelenaconZ <semartinez@fi.uba.ar>
2021-07-17 17:20:22 -0300

    Agrego Continente y Objetivo

---
+++ src/main/java/algoteg/Continente.java
@@ -0,0 +1,13 @@
+package algoteg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Continente {
+
+    List<Pais> paises = new ArrayList();
+
+    public Continente(){
+
+    }
+}
---
+++ src/main/java/algoteg/Objetivo.java
@@ -0,0 +1,9 @@
+package algoteg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Objetivo {
+    List<Pais> paises = new ArrayList<>();
+    List<Continente> continente = new ArrayList<Continente>();
+}
