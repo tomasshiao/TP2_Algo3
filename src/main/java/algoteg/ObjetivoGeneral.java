e0867b931dd7c8d38ebe24e701eb808fdcbc5257
MarcosCouttulenc <mcouttulenc@fi.uba.ar>
2021-07-21 17:08:58 -0300

    Creacion de clase ObjetivoGeneral y sus tests

---
+++ src/main/java/algoteg/ObjetivoGeneral.java
@@ -0,0 +1,32 @@
+package algoteg;
+
+
+public class ObjetivoGeneral implements Objetivo{
+    Jugador jugador;
+    String mensajeObjetivo;
+
+    public ObjetivoGeneral(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+
+    @Override
+    public boolean cumplido() {
+        return (this.jugador.getCantidadPaisesConquistados()  > 29);
+    }
+
+    @Override
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    @Override
+    public void setMensajeObjetivo(String mensajeObjetivo) {
+        this.mensajeObjetivo = mensajeObjetivo;
+    }
+
+    @Override
+    public String getMensajeObjetivo() {
+        return this.mensajeObjetivo;
+    }
+}
---
+++ src/main/test/algoteg/ObjetivoGeneralTest.java
@@ -0,0 +1,36 @@
+package algoteg;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class ObjetivoGeneralTest {
+    Jugador jugador = new Jugador(1,"rojo");
+    Jugador otroJugador = new Jugador(2, "azul");
+
+    @Test
+    public void cumpleObjetivoDominarTreintaPaises() {
+        for(int i = 0; i < 30; i++) {
+            Pais paisMock = mock(Pais.class);
+            jugador.addPaisConquistado(paisMock);
+        }
+
+        Objetivo objetivo = new ObjetivoGeneral(jugador);
+        assertTrue(objetivo.cumplido());
+    }
+
+    @Test
+    public void noCumpleObjetivoDominarTreintaPaises() {
+        for(int i = 0; i < 20; i++) {
+            Pais paisMock = mock(Pais.class);
+            otroJugador.addPaisConquistado(paisMock);
+        }
+
+        Objetivo otroObjetivo = new ObjetivoGeneral(otroJugador);
+
+        assertFalse(otroObjetivo.cumplido());
+    }
+}
