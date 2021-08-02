e78dbe8e18fefdf12371e40bca7b2c9d5567351d
selene <semartinez@fi.uba.ar>
2021-08-02 13:05:11 -0300

    cambios en el mapa

--- src/main/java/algoteg/controlador/IniciadorJugadores.java
+++ src/main/java/algoteg/controlador/IniciadorJugadores.java
@@ -1,14 +1,11 @@
 package algoteg.controlador;
-
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -16,26 +13,45 @@ import javafx.stage.Window;
 import java.io.IOException;
 
 public class IniciadorJugadores {
+    @FXML
+    public void initialize(){
+        toggleGroup = new ToggleGroup();
+        op1.setToggleGroup(toggleGroup);
+        op2.setToggleGroup(toggleGroup);
+        op3.setToggleGroup(toggleGroup);
+        op4.setToggleGroup(toggleGroup);
+        op5.setToggleGroup(toggleGroup);
+
+    }
 
     @FXML public Button aceptar;
-    @FXML public TextField cantJugadores;
+    @FXML public Label cantJugadores;
+    @FXML public RadioButton op1;
+    @FXML public RadioButton op2;
+    @FXML public RadioButton op3;
+    @FXML public RadioButton op4;
+    @FXML public RadioButton op5;
+    private ToggleGroup toggleGroup;
 
-     @FXML public void handleEvent(ActionEvent event){   //lee el textfield y lo imprime por consola
-         int jugadores = 0;
-         if(event.getSource().equals(aceptar) & !cantJugadores.getText().isEmpty()) {
-             try{
-                 jugadores = Integer.parseInt(cantJugadores.getText());
-             }
-             catch (NumberFormatException e){
-                 e.printStackTrace();
-             }
 
-             if(jugadores>=2 & jugadores<=6) {
 
-                 this.cambiarScena(event);
+    @FXML public void handleEvent(ActionEvent event){
+        //lee el textfield y lo imprime por consola
 
-             }
-         }
+
+        int jugadores = 0;
+        if(event.getSource().equals(aceptar) & !cantJugadores.getText().isEmpty()) {
+            try{
+                jugadores = Integer.parseInt(cantJugadores.getText());
+            }
+            catch (NumberFormatException e){
+                e.printStackTrace();
+            }
+
+            this.cambiarScena(event);
+
+
+        }
 
     }
     public void cambiarScena(ActionEvent event){
@@ -62,5 +78,27 @@ public class IniciadorJugadores {
     }
 
 
+    @FXML public void actualizarLabel()
+    {
+        if(toggleGroup.getSelectedToggle().equals(this.op1)){
+            this.cantJugadores.setText("2");
 
+        }
+        else if(toggleGroup.getSelectedToggle().equals(this.op2)){
+            this.cantJugadores.setText("3");
+
+        }
+        else if(toggleGroup.getSelectedToggle().equals(this.op3)){
+            this.cantJugadores.setText("4");
+
+        }
+        else if(toggleGroup.getSelectedToggle().equals(this.op4)){
+            this.cantJugadores.setText("5");
+
+        }
+        else if(toggleGroup.getSelectedToggle().equals(this.op5)){
+            this.cantJugadores.setText("6");
+
+        }
+    }
 }
